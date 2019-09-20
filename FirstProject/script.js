b56a7853a255f939e8c830035623bf531be96d72
jyotisatale <jyotisatale@gmailcom>
2019-09-20 17:10:25 +0530

    secong commit

--- FirstProject/script.js
+++ FirstProject/script.js
@@ -140,17 +140,238 @@ else{
     console.log(firstName + 'is a man');
 }
 */
+
+
 /*************** 
 *Ternary operator*/
+/*
 var firstName = 'John';
 var age = 14;
 
 age >= 18 ? console.log(firstName + 'drinks beer.') : console.log(firstName + 'drinks juice');
  var drink = age >= 18 ? 'beer' : 'juice';
 
+ //Switch case
+ var job = 'instructor';
+ switch(job)
+ {
+     case 'teacher':
+            console.log(firstName + 'teacher kids how to code.');
+            break;
+     case 'instructor':
+            console.log(firstName + 'is instructor.');
+             break;
+     case 'driver': 
+             console.log(firstName + 'drives an uber in lisbon.');
+              break;  
+     case 'designer':
+             console.log(firstName + 'designs beautiful websites.');
+              break;
+     default:
+            console.log(firstName + 'does somethings else.');
+                    
+ }
+
+ switch (true){
+     case age < 13:
+            console.log(firstName + 'is a boy.');
+            break;
+     case age >= 13 && age < 20:
+            console.log(firstName + 'is a teenager.');
+            break;
+     case age >= 20 && age < 30:
+            console.log(firstName + 'is a young.');
+            break; 
+      default:
+            console.log(firstName + 'is a man.');
+                    
+ }
+
+ function calculateAge(birthYear)
+ {
+     return 2018-birthYear;
+ }
+ var ageJohn = calculateAge(1998);
+ var ageMike = calculateAge(1998);
+ var ageJone = calculateAge(1998);
+ console.log(ageJohn, ageMike,  ageJone);
+
+ function yearUtilRetirement(year, firstName)
+ {
+ var age = calculateAge(year);
+ var retirement = 65 - age;
+
+ if(retirement > 0)
+ {
+     console.log(firstName + 'retires in' + retirement + 'years.');
+ }
+ else{
+     console.log(firstName + 'retires in '+ retirement +'years.');
+    }
+ }
+
+ yearUtilRetirement(1990, 'John');
+ yearUtilRetirement(1948, 'Mike');
+ yearUtilRetirement(1990, 'Jone');
+*/
 
 
+ /* ***********************************
+ *Fuction Statement and Expretions
+ */
+/*
+var whatDoYouDo = function(job, firstName)
+{
+    switch(job)
+    {
+        case 'teacher':
+            return firstName + 'teaches kids how to code';
+          
+        case 'driver': 
+        return firstName + 'drives a cab in lisbon'; 
+        
+           
+        case 'designer': 
+        return firstName + 'designs beautiful websites'; 
+
+        default:
+        return firstName + ' does something';
+    }
+}
+
+console.log(whatDoYouDo('teacher' , 'John'));
+console.log(whatDoYouDo('designer' , 'Jone'));
+console.log(whatDoYouDo('retired' , 'Mark'));
+
+*/
+
+/****************** 
+*Arrays
+*/
+/** 
+var names = ['john', 'Mark', 'jone'];
+var year = new Array(1990, 1969, 1948);
+
+console.log(names[0]);
+console.log(names);
+console.log(names.length);
+
+//Mutate array data
+names[1] = 'ben';
+names[names.length] = 'Mary'
+console.log(names);
+
+//different data type
+var john = ['John', 'smith', 1990, 'teacher', false];
+john.push('blue');
+john.unshift('mr');
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+
+console.log(john);
+console.log(john.indexOf(23));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'john is not a designer' : 'john is a designer';
+console.log(isDesigner);
+*/
+
+/*
+*objects and properties 
+*/
+/** 
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false
+};
+
+console.log(john.firstName);
+console.log(john['lastName'])
+
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+//new obeject syntax
+var jane = new Object();
+jane.name = 'Jane' ;
+jane.birthYear = 1969 ;
+jane['lastName'] = 'Smith';
+console.log(jane);
+*/
 
 
+/*
+*objects and methods
+*/
+/*
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function(){
+       // return 2018 - this.birthYear;
+       this.age = 2018 - this.birthYear;
+    }
+};
+
+//console.log(john.calcAge());
+john.calcAge();
+console.log(john);
+*/
+/**** loop *******************************
+ ************* for loop************
+for (var i = 1; i<= 20; i++)
+{
+    console.log(i);
+}
+
+var john = ['john', 'Smith', 1990, 'designer', false];
+for(var i = 0; i<john.length; i++)
+{
+    console.log(john[i]);
+}
+
+//while loop
+var i = 0;
+while(i < john.length)
+{
+    console.log(john[i]);
+    i++
+}
+ */
+
+//continue and break statement
+var john = ['john', 'Smith', 1990, 'designer', false];
+for(var i = 0; i<john.length; i++)
+{
+    if(typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i=0; i<john.length; i++)
+{
+    if(typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+//looping backwards
+for (var i = john.length-1; i>=0; i--)
+{
+    console.log(john[i]);
+   
+}
 
 
